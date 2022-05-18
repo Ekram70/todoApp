e480401fc4e4df75131ce8a961a6442acc82ac7e
Ekram70 <ekramullah70@gmail.com>
2022-05-18 21:18:52 +0600

    need to work on edit btns

--- app.js
+++ app.js
@@ -1,108 +1,125 @@
-const toDoInput = document.getElementById("todo-input");
-const toDoBtn = document.getElementById("todo-btn");
-const toDoList = document.getElementById("todo-list");
-const completedList = document.getElementById("completed-list");
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("todo-btn");
+const todoList = document.getElementById("todo-list");
+const completeList = document.getElementById("completed-list");
 
-const todoItems = document.querySelectorAll(".todo-item");
-const editBtns = document.querySelectorAll(".edit");
-const completeBtns = document.querySelectorAll(".completed");
-const undoBtns = document.querySelectorAll(".undo");
-const deleteBtns = document.querySelectorAll(".deleted");
+let checkBtns = document.querySelectorAll(".completed");
+let undoBtns = document.querySelectorAll(".undo");
+let deleteBtns = document.querySelectorAll(".deleted");
 
-toDoBtn.addEventListener("click", addToList);
+addBtn.addEventListener("click", addToList);
 
-toDoInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    addToList();
-  }
+checkBtns.forEach((btn) => {
+  btn.addEventListener("click", addToComplete);
 });
 
-completeBtns.forEach((complete) => {
-  complete.addEventListener("click", addToComplete);
+undoBtns.forEach((btn) => {
+  btn.addEventListener("click", undoFn);
 });
 
-undoBtns.forEach((undo) => {
-  undo.addEventListener("click", undoFn);
+deleteBtns.forEach((btn) => {
+  btn.addEventListener("click", deleteFn);
 });
 
-deleteBtns.forEach((del) => {
-  del.addEventListener("click", () => {
-    del.parentElement.remove();
-  });
-});
-
-// utilities function
-let i = 1;
+let todoNum = 1;
 function addToList() {
-  if (toDoInput.value) {
-    if (toDoList.lastElementChild) {
-      let number = toDoList.lastElementChild.querySelector(".number").innerText;
-      i = Number(number) + 1;
-    }
-    toDoList.innerHTML += `<li class="todo-item">
-    <span class="number">${i < 10 ? "0" + i++ : i++}</span>
-    <div class="todo-text">${toDoInput.value}</div>
-    <span class="completed" title="complete"><i class="fa-solid fa-check"></i></span>
-    </li>`;
+  if (todoList.lastElementChild) {
+    let num = todoList.lastElementChild.firstElementChild.innerText;
+    todoNum = 1 + Number(num);
   }
 
-  let item = toDoList.lastChild;
+  if (todoInput.value) {
+    todoList.innerHTML += `
+    <li class="todo-item">
+      <span class="number">${todoNum < 10 ? "0" + todoNum++ : todoNum++}</span>
+      <div class="todo-text">${todoInput.value}</div>
+      <span class="edit" title="edit">
+        <i class="fa-solid fa-pencil"></i>
+      </span>
+      <span class="completed" title="complete">
+        <i class="fa-solid fa-check"></i>
+      </span>
+    </li>
+    `;
+  }
 
-  let completeBtn = item.querySelector(".completed");
-  completeBtn.addEventListener("click", addToComplete);
+  let checkBtns = document.querySelectorAll(".completed");
+  checkBtns.forEach((btn) => {
+    btn.addEventListener("click", addToComplete);
+  });
 }
 
-let k = 1;
+let completeNum = 1;
 function addToComplete() {
+  if (completeList.lastElementChild) {
+    let num = completeList.lastElementChild.firstElementChild.innerText;
+    completeNum = 1 + Number(num);
+  }
+
   let parent = this.parentElement;
   let text = parent.querySelector(".todo-text").innerText;
 
-  if (completedList.lastElementChild) {
-    let number =
-      completedList.lastElementChild.querySelector(".number").innerText;
-    k = Number(number) + 1;
-  }
-  completedList.innerHTML += `<li class="completed-item">
-  <span class="number">${k < 10 ? "0" + k++ : k++}</span>
-  <div class="completed-text">${text}</div>
-  <span class="undo" title="undo"><i class="fas fa-hand-point-up"></i></span>
-  </li>`;
+  completeList.innerHTML += `
+  <li class="completed-item">
+    <span class="number">
+    ${completeNum < 10 ? "0" + completeNum++ : completeNum++}
+    </span>
+    <div class="completed-text">${text}</div>
+    <span class="undo" title="undo">
+      <i class="fas fa-hand-point-up"></i>
+    </span>
+    <span class="deleted" title="delete">
+      <i class="fas fa-trash"></i>
+    </span>
+  </li>
+  `;
 
-  let item = completedList.lastChild;
+  let undoBtns = document.querySelectorAll(".undo");
+  undoBtns.forEach((btn) => {
+    btn.addEventListener("click", undoFn);
+  });
 
-  let undoBtn = item.querySelector(".undo");
-  undoBtn.addEventListener("click", undoFn);
+  let deleteBtns = document.querySelectorAll(".deleted");
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", deleteFn);
+  });
+
   parent.remove();
 }
 
+let undoNum = 1;
 function undoFn() {
+  if (todoList.lastElementChild) {
+    let num = todoList.lastElementChild.firstElementChild.innerText;
+    undoNum = 1 + Number(num);
+  }
+
   let parent = this.parentElement;
-  let text = this.previousElementSibling.innerText;
-  toDoList.innerHTML += `<li class="todo-item">
-    <span class="number">01</span>
+  let text = parent.querySelector(".completed-text").innerText;
+
+  todoList.innerHTML += `
+  <li class="todo-item">
+    <span class="number">${undoNum < 10 ? "0" + undoNum++ : undoNum++}</span>
     <div class="todo-text">${text}</div>
-    <span class="completed" title="complete"><i class="fa-solid fa-check"></i></span>
-    </li>`;
+    <span class="edit" title="edit">
+      <i class="fa-solid fa-pencil"></i>
+    </span>
+    <span class="completed" title="complete">
+      <i class="fa-solid fa-check"></i>
+    </span>
+  </li>
+  `;
 
-  let item = toDoList.lastChild;
-
-  let completeBtn = item.querySelector(".completed");
-  completeBtn.addEventListener("click", addToComplete);
+  let checkBtns = document.querySelectorAll(".completed");
+  checkBtns.forEach((btn) => {
+    btn.addEventListener("click", addToComplete);
+  });
 
   parent.remove();
 }
 
-// editBtns.forEach((edit) => {
-//   edit.addEventListener("click", function () {
-//     edit.previousElementSibling.setAttribute("contenteditable", "true");
-//     edit.previousElementSibling.className += " editable-text";
-//   });
-// });
+function deleteFn() {
+  let parent = this.parentElement;
 
-// window.addEventListener("click", (e) => {
-//   if (e.target.parentElement != edit.parentElement) {
-//     edit.previousElementSibling.setAttribute("contenteditable", "false");
-//     edit.previousElementSibling.className = "todo-text";
-//   }
-// });
+  parent.remove();
+}
